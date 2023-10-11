d21764548560f2e9bb9d0fff8040303bf8aa686d
RayPlante <raymond.plante@nist.gov>
2023-10-11 11:25:57 -0400

    set up modules

---
+++ python/nistoar/auth/idp/__init__.py
@@ -0,0 +1,6 @@
+"""
+a module providing support for interacting with an identity provider (IDP).  
+Support for different IDPs appear in different submodules.  The primary 
+function of the different implementations is to populate a Credentials 
+instance from the data returned by the IDP.
+"""
---
+++ python/nistoar/auth/wsgi/__init__.py
@@ -0,0 +1,8 @@
+"""
+a module providing the implementations for the OAR authentication web service.  
+The purpose of the service is to broker the authentication process with the IDP 
+and to provide authentication tokens for authenticated users that need to 
+connect to other OAR services.  
+"""
+
+# the default implementation is a flask app
