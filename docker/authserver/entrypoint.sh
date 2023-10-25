1b65c9ad93b8372e9ba5ac2affaeddafcaa4a6e2
RayPlante <raymond.plante@nist.gov>
2023-10-25 15:27:28 -0400

    docker/authserver: switch to HTTPS

--- docker/authserver/entrypoint.sh
+++ docker/authserver/entrypoint.sh
@@ -17,9 +17,13 @@ echo Working Dir: $OAR_WORKING_DIR
 echo Access the OAR Authentication broker service at http://localhost:$port/
 echo
 
-echo '++' uwsgi --plugin python3 --http-socket :$port --wsgi-file $script --static-map /docs=/docs \
+crts=/app/dist/oarauth/etc/authservice/certs
+
+echo '++' uwsgi --plugin python3 --https-socket :$port,$crts/spsite.pem,$crts/spsite.key \
+                --wsgi-file $script --static-map /docs=/docs \
                 --set-ph oar_config_file=$OAR_AUTHSERVER_CONFIG \
                 --set-ph oar_working_dir=$OAR_WORKING_DIR $opts
-uwsgi --plugin python3 --http-socket :$port --wsgi-file $script --static-map /docs=/docs \
-     --set-ph oar_config_file=$OAR_AUTHSERVER_CONFIG \
-     --set-ph oar_working_dir=$OAR_WORKING_DIR $opts
+uwsgi --plugin python3 --https-socket :$port,$crts/spsite.pem,$crts/spsite.key \
+      --wsgi-file $script --static-map /docs=/docs \
+      --set-ph oar_config_file=$OAR_AUTHSERVER_CONFIG \
+      --set-ph oar_working_dir=$OAR_WORKING_DIR $opts
